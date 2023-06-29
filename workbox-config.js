168f94b60f219295e4732d4963fe74e8c55c3116
Jenish Jerome <9320926+jenishngl@users.noreply.github.com>
2023-06-29 16:05:43 +0530

    Updates code

---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "globDirectory": "./",
+    "globPatterns": [
+        "**/*.{html,js,css,png,svg,webp,webmanifest}"
+    ],
+    "swDest": "./sw/sw.js",
+    "skipWaiting": true,
+    "clientsClaim": true
+};
